e9c4cb28fb6d30255bc4f9790d2d699440a6a0c7
mayanksrivastava1 <100144534+mayanksrivastava1@users.noreply.github.com>
2023-08-25 18:47:50 +0530

    Ejs

--- data.js
+++
@@ -1,8 +0,0 @@
-const data = [
-    {name:'MAyank', email:'MAyank@gmail.com'},
-    {name:'Om', email:'om@gmail.com'},
-    {name:'Shreya', email:'shreya@gmail.com'}
-]
-
-
-module.exports = data
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const http = require('http')
-const data = require('./data')
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type' : 'application\json'}),
-    res.write(JSON.stringify(data))
-    res.end()
-}).listen(5000)
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const publicPath = path.join(__dirname,'public')
+
+// app.use(express.static(publicPath))
+
+app.get('',(req,res)=>{
+    res.sendFile(`${publicPath}/Home.html`)
+})
+app.get('/about',(req,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+//
+app.get('*',(req,res)=>{
+    res.sendFile(`${publicPath}/Erroe404.html`)
+})
+
+app.listen(5000)
+
